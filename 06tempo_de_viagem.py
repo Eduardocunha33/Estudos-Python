f7522120317bfcc5c28ae12d3ca06910a650e1ad
Eduardo Lima Cunha <limaeduardo008@gmail.com>
2026-08-27 21:14:11 -0300

    implementação de função

--- 06tempo_de_viagem.py
+++ 06tempo_de_viagem.py
@@ -2,10 +2,13 @@ tempo = 60
 distancia = float(input("Qual a distância que será percorrida em km: "))
 velocidadeMedia = int(input("Qual será a velocidade média durante o trajeto em km/h? "))
 
-hora = distancia / velocidadeMedia
+def duracaoViagem(tempo, distancia, velocidadeMedia):
+    hora = distancia / velocidadeMedia
 
-tempoFinal = tempo * hora
-if tempoFinal < 60:
-    print(f"tempo levado para completar essa trajetória será de: {tempoFinal:.2f} minutos")
-elif tempoFinal >= 60:
-    print(f"O tempo levado para completar essa trajetória será de: {tempoFinal/60} horas")
+    tempoFinal = tempo * hora
+    if tempoFinal < 60:
+        return(f"tempo levado para completar essa trajetória será de: {tempoFinal:.2f} minutos")
+    elif tempoFinal >= 60:
+        return(f"O tempo levado para completar essa trajetória será de: {tempoFinal/60} horas")
+
+print(duracaoViagem(60, distancia, velocidadeMedia))
