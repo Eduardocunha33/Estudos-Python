38e043bd3227440ecc50672001d738c28f9d3b53
Eduardo Lima Cunha <limaeduardo008@gmail.com>
2026-08-31 19:09:35 -0300

    número maior ou igual

---
+++ 11numero_maior.py
@@ -0,0 +1,9 @@
+a = int(input("Digite o primeiro número: "))
+b = int(input("Digite o primeiro número: "))
+
+if a > b:
+    print("O primeiro número é maior!")
+elif b > a:
+    print("O segundo número é maior")
+elif a == b:
+    print("Os números tem o mesmo valor!")
